5b8136c5ee930b1097e13dea0d5266a101075eac
PLunaM-030 <L25550696@chihuahua2.tecnm.mx>
2025-08-25 22:39:25 -0700

    Add files via upload
    this is your.. uhh first work, first activity..

---
+++ EVA1-isc-PaulinaILM/src/eva1/isc/paulinailm/EVA1IscPaulina_holamundoJava.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1.isc.paulinailm;
+
+/**
+ *
+ * @author pawlu
+ */
+public class EVA1IscPaulinaILM {
+    
+    public static void main (String[] args) {
+        System.out.println("Say hi, world!!1!1!!");
+        System.out.println("We are looking to code, this is 'Fundamentos de programacion'.");
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+        // TODO code application logic here
+    }
